cd9bee14b8c42d335f92e4eeb83285ac3f546fac
salivo <andrijsalivon@gmail.com>
2025-08-11 22:14:44 +0200

    Brake and coast

--- campone/motion.py
+++ campone/motion.py
@@ -2,6 +2,10 @@ import Jetson.GPIO as GPIO
 import spidev
 
 MOTOR_SPEED_BASE = 0X1A
+MOTOR_POSITION_BASE = 0X1B
+MOTORS_COAST = 0X30
+MOTORS_BRAKE = 0X30
+
 CS_PIN = 8
 
 GPIO.setwarnings(False)
@@ -24,9 +28,6 @@ class Motion:
         self.spi.max_speed_hz = 500000   # 500 kHz
         self.spi.mode = 0
 
-    def get_board_fw_version(self):
-        self.spi.xfer([0x00, 0x00, 0x00, 0x00])
-
     def get_board_version(self):
         self.spi.xfer([0x00, 0x00, 0x00, 0x00])
 
@@ -35,8 +36,17 @@ class Motion:
         speed_16 = speed & 0xFFFF
         spi_transfer(self.spi, [MOTOR_SPEED_BASE + 0X10 * motor, (speed_16 >> 8) & 0xFF, speed_16 & 0xFF])
 
-    def set_motor_position(self, motor, position):
-        self.spi.xfer([0x00, 0x00, 0x00, 0x00])
+    def set_motor_position(self, motor, position, max_speed):
+        if motor > 1 : return
+        speed_16 = max_speed & 0xFFFF
+        position_8 = position & 0xFF
+        spi_transfer(self.spi, [MOTOR_POSITION_BASE + 0X10 * motor, position_8, (speed_16 >> 8) & 0xFF, speed_16 & 0xFF])
+
+    def coast_motors(self):
+        spi_transfer(self.spi, [MOTORS_COAST])
+
+    def brake_motors(self):
+        spi_transfer(self.spi, [MOTORS_BRAKE])
 
     def __del__(self):
         self.spi.close()
--- test_motion.py
+++ test_motion.py
@@ -1,5 +1,8 @@
 import campone
+import time
 
 motion = campone.Motion()
-motion.set_motor_speed(motion.LEFT, -500)
-motion.set_motor_speed(motion.RIGHT, -500)
+motion.set_motor_speed(motion.LEFT, 500)
+motion.set_motor_speed(motion.RIGHT, 500)
+time.sleep(1)
+motion.brake_motors()
