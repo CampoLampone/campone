c89536befaab2d06dd2bdaef2b1d5fee1688c333
Vesek <venda.straka@gmail.com>
2026-08-12 16:02:26 +0200

    Update test_motion.py

--- tests/test_motion.py
+++ tests/test_motion.py
@@ -2,10 +2,20 @@ import campone
 import time
 
 motion = campone.Motion()
-motion.set_motor_speed(motion.LEFT, -150)
-motion.set_motor_speed(motion.RIGHT, 150)
-time.sleep(3)
-motion.set_motor_speed(motion.LEFT, 150)
-motion.set_motor_speed(motion.RIGHT, -150)
-time.sleep(3)
+
+test_speed = 100
+
+# You can set PID coefficients right here
+# motion.set_pid_coeffs(3, 2, 1)
+
+# Forward
+motion.set_motor_speed(motion.LEFT, -test_speed)
+motion.set_motor_speed(motion.RIGHT, test_speed)
+time.sleep(2)
+motion.brake_motors()
+time.sleep(1)
+# Back
+motion.set_motor_speed(motion.LEFT, test_speed)
+motion.set_motor_speed(motion.RIGHT, -test_speed)
+time.sleep(2)
 motion.brake_motors()
