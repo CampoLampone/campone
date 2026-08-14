b744dcbded48f8093922b37a6ec935567ba4c992
Vesek <venda.straka@gmail.com>
2026-08-15 00:01:31 +0200

    Add a receiving UDP socket
    Maximum 16 bytes messages for controlling
    
    Set maximum queue size

--- main.py
+++ main.py
@@ -3,13 +3,15 @@ import queue
 
 from workers.camera import CameraCapture
 from workers.lane_follower import LaneFollower
+from workers.udp_socket import UdpSocket, UdpPacket
 
 if __name__ == "__main__":
     cam = CameraCapture()
     writer = campone.UDPWriter()
-    command_queue = queue.Queue()
+    command_queue = queue.Queue(maxsize=100)
     lf = LaneFollower(cam, writer, command_queue)
     motion = campone.Motion()
+    sock = UdpSocket(command_queue)
 
     motion.set_pid_coeffs(3, 2, 0.4)
 
@@ -27,6 +29,10 @@ if __name__ == "__main__":
                     motors_setpoint = motors
                     motion.set_motor_speed(motion.LEFT, -motors_setpoint[1])
                     motion.set_motor_speed(motion.RIGHT, motors_setpoint[0])
+
+            elif source == 'udp_socket':
+                packet: UdpPacket = payload
+                print(f"{packet.data} from {packet.address}") # Replace this with your solution
 
     except KeyboardInterrupt:
         cam.stop()
---
+++ workers/udp_socket.py
@@ -0,0 +1,40 @@
+import socket
+import threading
+import queue
+from dataclasses import dataclass
+
+@dataclass
+class UdpPacket:
+    data: bytes = b''
+    address: str = "0.0.0.0"
+
+class UdpSocket:
+    def __init__(self, command_queue):
+        self.host = '0.0.0.0'
+        self.port = 6969
+        self.command_queue = command_queue
+        self.max_message_size = 16
+
+        self.thread = threading.Thread(target=self.run, daemon=True)
+        self.running = True
+        self.thread.start()
+
+    def run(self):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        sock.bind((self.host, self.port))
+        sock.settimeout(1.0) # so we can check self.running at least once every second
+        while self.running:
+            try:
+                data, address = sock.recvfrom(self.max_message_size)
+                try:
+                    self.command_queue.put_nowait(('udp_socket', UdpPacket(data=data, address=address[0])))
+                except queue.Full:
+                    pass # probably getting spammed by some very funny participant
+            except TimeoutError:
+                continue
+
+        sock.close()
+
+    def __del__(self):
+        self.running = False
+        self.thread.join()
